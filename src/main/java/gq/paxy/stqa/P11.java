b29f91dcc4f77bddadb5e15ac159bf551b3e00f7
Paxyifier <pingale2001@outlook.com>
2023-10-18 22:17:08 +0530

    Done with P13

---
+++ src/main/java/gq/paxy/stqa/P11.java
@@ -0,0 +1,42 @@
+package gq.paxy.stqa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.WebElement;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+
+public class P11 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		WebDriver driver = new ChromeDriver();
+
+        // Navigate to the jQuery UI Demo website
+        driver.get("https://jqueryui.com/droppable/");
+        Thread.sleep(5000);
+
+        // Switch to the frame that contains the interactive demo
+        driver.switchTo().frame(driver.findElement(By.className("demo-frame")));
+
+        // Initialize a new Actions instance
+        Actions actions = new Actions(driver);
+
+        // Find the source and target elements
+        WebElement sourceElement = driver.findElement(By.id("draggable"));
+        WebElement targetElement = driver.findElement(By.id("droppable"));
+
+        // Perform the drag and drop action
+        actions.dragAndDrop(sourceElement, targetElement).perform();
+        Thread.sleep(5000);
+
+        // Switch back to the default content
+        driver.switchTo().defaultContent();
+        Thread.sleep(5000);
+
+        // Close the browser
+        driver.quit();
+	}
+}
---
+++ src/main/java/gq/paxy/stqa/P13.java
@@ -0,0 +1,51 @@
+package gq.paxy.stqa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class P13 {
+	WebDriver driver;
+
+	@Test(dataProvider = "testdata")
+	public void demoClass(String email, String password) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		driver = new ChromeDriver();
+
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(5000);
+		driver.findElement(By.name("username")).sendKeys(email);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.cssSelector("button[type=submit]")).click();
+		Thread.sleep(5000);
+		;
+		Assert.assertTrue(driver.getCurrentUrl().matches("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"), "Invalid credentials");
+		System.out.println("Login successful");
+	}
+
+	@AfterMethod
+	void ProgramTermination() {
+		driver.quit();
+	}
+
+	@DataProvider(name = "testdata")
+	public Object[][] testDataExample() {
+		ReadExcelFile configuration = new ReadExcelFile("/home/paxy/eclipse-workspace/stqa/p13test.xlsx");
+
+		int rows = configuration.getRowCount(0);
+
+		Object[][] signin_credentials = new Object[rows][2];
+
+		for (int i = 0; i < rows; i++) {
+			signin_credentials[i][0] = configuration.getData(0, i, 0);
+			signin_credentials[i][1] = configuration.getData(0, i, 1);
+		}
+		return signin_credentials;
+	}
+
+}
